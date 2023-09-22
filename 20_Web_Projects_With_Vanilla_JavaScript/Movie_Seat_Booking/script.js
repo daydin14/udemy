2b460ed137179a352feb91829e738b4be79227aa
David Aydin <david.aydin6@gmail.com>
2023-09-22 16:41:34 -0400

    20 web proj vanilla js, Movie seat booking populate ui with saved data

--- 20_Web_Projects_With_Vanilla_JavaScript/Movie_Seat_Booking/script.js
+++ 20_Web_Projects_With_Vanilla_JavaScript/Movie_Seat_Booking/script.js
@@ -4,6 +4,8 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
+populateUI();
+
 let ticketPrice = +movieSelect.value; //  or parseInt(movieSelect.value)
 
 // Save selected movie index and price
@@ -24,9 +26,27 @@ function updateSelectedCount() {
     total.innerText = selectedSeatsCount * ticketPrice;
 }
 
+// Get data from local storage and populate the UI
+function populateUI() {
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+    if (selectedSeats !== null && selectedSeats.length > 0) {
+        seats.forEach((seat, index) => {
+            if (selectedSeats.indexOf(index) > -1) {
+                seat.classList.add('selected');
+            }
+        })
+    }
+
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+    if (selectedMovieIndex !== null) {
+        movieSelect.selectedIndex = selectedMovieIndex;
+    }
+}
+
 // Movie select event
 movieSelect.addEventListener('change', (e) => {
     ticketPrice = +e.target.value;
+    setMovieData(e.target.selectedIndex, e.target.value);
     updateSelectedCount();
 });
 
@@ -38,3 +58,6 @@ container.addEventListener('click', (e) => {
         updateSelectedCount();
     }
 })
+
+// Set initial count and total
+updateSelectedCount();
