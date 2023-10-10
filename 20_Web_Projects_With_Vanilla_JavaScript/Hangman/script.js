0ac373d3e7b6638af90e00dc52a170ef9c06d2a8
David Aydin <david.aydin6@gmail.com>
2023-10-10 10:05:44 -0400

    Handled wrong letters and play again win/lose

--- 20_Web_Projects_With_Vanilla_JavaScript/Hangman/script.js
+++ 20_Web_Projects_With_Vanilla_JavaScript/Hangman/script.js
@@ -1,6 +1,6 @@
 const wordEl = document.getElementById('word');
 const wrongLettersEl = document.getElementById('wrong-letters');
-const playAgainBtn = document.getElementById('play-again');
+const playAgainBtn = document.getElementById('play-button');
 const popup = document.getElementById('popup-container');
 const notificaiton = document.getElementById('notification-container');
 const finalMessage = document.getElementById('final-message');
@@ -38,7 +38,27 @@ function displayWord() {
 
 // Update the wrong letters 
 function updateWrongLettersEl() {
+    // Display wrong letters
+    wrongLettersEl.innerHTML = `
+        ${wrongLetters.length > 0 ? '<p>Wrong</p>' : ''}
+        ${wrongLetters.map(letter => `<span>${letter}</span>`)}
+    `;
 
+    // Display parts
+    figureParts.forEach((part, index) => {
+        const errors = wrongLetters.length;
+        if (index < errors) {
+            part.style.display = 'block';
+        } else {
+            part.style.display = 'none';
+        }
+    });
+
+    // Check if lost
+    if (wrongLetters.length === figureParts.length) {
+        finalMessage.innerText = 'Unfortunately you lost.';
+        popup.style.display = 'flex';
+    };
 }
 
 // Show Notification
@@ -51,12 +71,11 @@ function showNotification() {
 
 // Keydown letter press
 window.addEventListener('keydown', e => {
-    console.log(e.key)
-    console.log(typeof (e.key))
+    // console.log(e.key)
+    // console.log(typeof (e.key))
 
-    // Deprecated
-    console.log(e.keyCode)
-
+    //console.log(e.keyCode)
+    // keyCode is DEPRECATED!!!!
     if (e.keyCode >= 65 && e.keyCode <= 90) {
         const letter = e.key;
         if (selectedWord.includes(letter)) {
@@ -77,5 +96,18 @@ window.addEventListener('keydown', e => {
     }
 })
 
+// Restart game and play again
+playAgainBtn.addEventListener('click', () => {
+    // Empty arrays
+    correctLetters.splice(0);
+    wrongLetters.splice(0);
+    // Select new word and display it
+    selectedWord = words[Math.floor(Math.random() * words.length)];
+    displayWord();
+    // Displays newly emptied wrong letters
+    updateWrongLettersEl();
+    // Hides pop win/lose box
+    popup.style.display = 'none';
+});
 
 displayWord();
