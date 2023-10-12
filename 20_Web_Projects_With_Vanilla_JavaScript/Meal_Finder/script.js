feed25d1fbbc635468ad817e44b50f2fb9f49056
David Aydin <david.aydin6@gmail.com>
2023-10-12 18:22:45 -0400

    Fixed meal img layering

--- 20_Web_Projects_With_Vanilla_JavaScript/Meal_Finder/script.js
+++ 20_Web_Projects_With_Vanilla_JavaScript/Meal_Finder/script.js
@@ -29,7 +29,7 @@ function searchMeal(e) {
                     mealsEl.innerHTML = data.meals.map(meal => `
                         <div class="meal">
                             <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
-                            <div class="meal-info" data-mealID="${meal.idMeal}>
+                            <div class="meal-info" data-mealID="${meal.idMeal}">
                                 <h3>${meal.strMeal}</h3>
                             </div>
                         </div>
