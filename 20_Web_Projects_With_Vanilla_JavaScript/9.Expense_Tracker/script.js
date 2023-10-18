aaa8d5d318a63da36ec83b7b4efdd440e220d60e
David Aydin <david.aydin6@gmail.com>
2023-10-18 10:58:03 -0400

    display balance, income, & expense

--- 20_Web_Projects_With_Vanilla_JavaScript/9.Expense_Tracker/script.js
+++ 20_Web_Projects_With_Vanilla_JavaScript/9.Expense_Tracker/script.js
@@ -26,10 +26,37 @@ function addTransactionDOM(transaction) {
     list.appendChild(item);
 }
 
+// Update balance, income, & expense
+function updateValues() {
+    const amounts = transactions.map(transaction => transaction.amount);
+    const total = amounts
+        .reduce((acc, item) => (acc += item), 0)
+        .toFixed(2);
+    const income = amounts
+        .filter(item => item > 0)
+        .reduce((acc, item) => (acc += item), 0)
+        .toFixed(2);
+    const expense = (amounts
+        .filter(item => item < 0)
+        .reduce((acc, item) => (acc += item), 0) * -1)
+        .toFixed(2);
+
+    // Debugging:
+    // console.log(amounts)
+    // console.log(total)
+    // console.log(income)
+    // console.log(expense)
+
+    balance.innerText = `$${total}`;
+    money_plus.innerText = `$${income}`;
+    money_minus.innerText = `$${expense}`;
+}
+
 // Init app
 function init() {
     list.innerHTML = '';
     transactions.forEach(addTransactionDOM)
+    updateValues();
 }
 
 init();
