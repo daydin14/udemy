fa932be77ac892fd57284fa3a8f371120c4cbf54
David <david.aydin6@gmail.com>
2023-10-29 18:58:29 -0400

    Adding a controller

---
+++ ASP.NET_Core6_Course-MVC-Blazor-Razor-EF-Core/FirstMVC/FirstMVC/Controllers/HelloWorldController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FirstMVC.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public string Index()
+        {
+            return "This is the Index page!";
+        }
+
+        public string Hello()
+        {
+            return "Who's there?";
+        }
+    }
+}
+
