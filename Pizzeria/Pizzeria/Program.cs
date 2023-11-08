5a2f8de81a03ce5b7783d5b89eceada847f590b4
David <david.aydin6@gmail.com>
2023-11-07 23:29:57 -0500

    Removed wrong template, now using .NET 6 ASP Web App razor

--- Pizzeria/Pizzeria/Program.cs
+++ Pizzeria/Pizzeria/Program.cs
@@ -1,13 +1,7 @@
-﻿using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
-using Pizzeria.Data;
-
-var builder = WebApplication.CreateBuilder(args);
+﻿var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
 
 var app = builder.Build();
 
@@ -20,13 +14,13 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
 
 app.UseRouting();
 
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
+app.UseAuthorization();
+
+app.MapRazorPages();
 
 app.Run();
 
